d0440a039b7804613a284d7ab2efd8bafbb813fa
Gustavo Taima <gustavotaima18@gmail.com>
2022-05-03 15:52:30 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const menuButton = document.getElementById('btn-menu')
+const menuButton = document.querySelector('#btn-menu')
 const header = document.querySelector('#header')
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') {
         event.preventDefault()
     }
-    const nav = document.getElementById('nav')
+    const nav = document.querySelector('#nav')
+    const header = document.querySelector('#header')
+    const main = document.querySelector('#main')
     nav.classList.toggle('active')
-    header.style.display = 'fixed'
+    main.classList.toggle('active')
+    header.classList.toggle('active')
 }
 
 menuButton.addEventListener('click', toggleMenu)
